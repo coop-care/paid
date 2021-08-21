0f6fc07bbef304061081ffe4cf9e08af850ad2b5
Tobias Zwick <newton@westnordost.de>
2021-08-21 19:33:53 +0200

    add validation stuff

--- src/sgb-v/validation.ts
+++ src/sgb-v/validation.ts
@@ -1,137 +1,79 @@
 import { 
-    Abrechnungsfall,
+    Umsatzsteuer,
+    Versicherter
+} from "../types"
+import { 
+    constraintsInstitution, 
+    constraintsVersicherter, 
+    constraintsUmsatzsteuer, 
+    constraintsIKToSondertarif
+} from "../validation"
+import { 
+    isRequired, isDate, isArray, isVarchar, isNumber, isOptionalInt, isInt,
+    isTruncatedIfTooLong, isOptionalVarchar,
+    arrayConstraints, valueConstraints,
+} from "../validation/utils"
+import { 
+    BaseAbrechnungsfall,
+    BaseAbrechnungsposition,
     Diagnose,
-    Einsatz,
     Kostenzusage,
+    Leistungserbringer,
+    Skonto,
     Verordnung, 
 } from "./types"
 
-const constraintsAbrechnungsfall = (a: Abrechnungsfall) => [
-    maxTextLength(a, "belegnummer", 10),
-    maxTextLength(a, "besondereVersorgungsform", 25),
+// Einzelrechnung, Rechnung, Sammelrechnung are not validated because they will be just intermediate
+// data structures, not exposed to the user of the library
+
+export const constraintsLeistungserbringer = (l: Leistungserbringer) => [
+    ...constraintsInstitution(l),
+    isRequired(l, "abrechnungscode"),
+    isRequired(l, "tarifbereich"),
+    isRequired(l, "location"),
+    isRequired(l, "sondertarifJeKostentraegerIK"),
+    ...valueConstraints<Record<string, string>>(l, "sondertarifJeKostentraegerIK", constraintsIKToSondertarif),
+    ...valueConstraints<Umsatzsteuer>(l, "umsatzsteuer", constraintsUmsatzsteuer)
+]
+
+export const constraintsBaseAbrechnungsfall = (a: BaseAbrechnungsfall) => [
+    isRequired(a, "versicherter"), 
     ...valueConstraints<Versicherter>(a, "versicherter", constraintsVersicherter),
-    notEmpty(a, "einsaetze"),
-    ...arrayConstraints<Einsatz>(a, "einsaetze", constraintsEinsatz),
-    notEmpty(a, "verordnungen"),
-    ...arrayConstraints<Verordnung>(a, "verordnungen", constraintsVerordnung),
+    isVarchar(a, "belegnummer", 10),
+    // beleginformation is optional
+    isOptionalVarchar(a, "besondereVersorgungsform", 25)
 ]
 
-const constraintsVersicherter = (v: Versicherter) => [
-    maxTextLength(v, "versichertennummer", 12),
-    // if versichertennummer or versichertenstatus is not specified, address is mandatory
-    !!v.versichertennummer && !!v.versichertenstatus || requireValue(v, "address"),
-    textNotTruncated(v, "firstName", 30),
-    textNotTruncated(v, "lastName", 47),
-    ...valueConstraints<Address>(v, "address", constraintsAddress),
+export const constraintsBaseAbrechnungsposition = (a: BaseAbrechnungsposition) => [
+    isNumber(a, "einzelpreis", 0, 1e10),
+    isNumber(a, "anzahl", 1, 1e4),
+    isOptionalInt(a, "gefahreneKilometer", 0, 1e6),
+    isTruncatedIfTooLong(isOptionalVarchar(a, "text", 70))
 ]
 
-const constraintsAddress = (a: Address) => [
-    textNotTruncated(a, "street", 24), // street + housenumber is max 30
-    textNotTruncated(a, "houseNumber", 5),
-    textNotTruncated(a, "postalCode", 7),
-    textNotTruncated(a, "city", 25),
-]
-
-const constraintsVerordnung = (v: Verordnung) => [
-    maxTextLength(v, "betriebsstaettennummer", 9),
-    maxTextLength(v, "vertragsarztnummer", 9),
+export const constraintsVerordnung = (v: Verordnung) => [
+    isOptionalVarchar(v, "betriebsstaettennummer", 9),
+    isOptionalVarchar(v, "vertragsarztnummer", 9),
+    isDate(v, "verordnungsDatum"),
+    // unfall, sonstigeEntschaedigung, verordnungsBesonderheiten are optional
+    isArray(v, "diagnosen", 0),
     ...arrayConstraints<Diagnose>(v, "diagnosen", constraintsDiagnose),
-    notEmpty(v, "kostenzusagen"),
+    isArray(v, "kostenzusagen", 1),
     ...arrayConstraints<Kostenzusage>(v, "kostenzusagen", constraintsKostenzusage),
 ]
 
 const constraintsDiagnose = (d: Diagnose) => [
-    maxTextLength(d, "diagnoseschluessel", 12),
-    textNotTruncated(d, "diagnosetext", 70)
+    isOptionalVarchar(d, "diagnoseschluessel", 12),
+    isTruncatedIfTooLong(isOptionalVarchar(d, "diagnosetext", 70))
 ]
 
 const constraintsKostenzusage = (k: Kostenzusage) => [
-    maxTextLength(k, "genehmigungsKennzeichen", 20)
+    isVarchar(k, "genehmigungsKennzeichen", 20),
+    isDate(k, "genehmigungsDatum"),
+    isRequired(k, "kostenzusageGenehmigung")
 ]
 
-
-const error = (code: ValidationCode, property: string, params?: Record<string, string>): ValidationResult => ({
-    code, type: ValidationResultType.Error, path: [property], params
-})
-
-export const notEmpty = <T>(obj: T, key: keyof T): ValidationResult | true => 
-    obj[key] === undefined || obj[key].length > 0 || 
-    error("arrayIsEmpty", key)
-
-export const maxTextLength = (obj: any, key: string, maxLength: number): ValidationResult | true =>
-    obj[key] === undefined || obj[key].length <= maxLength || 
-    error("textIsTooLong", key, { maxLength: maxLength.toString() })
-
-export const textNotTruncated = (obj: any, key: string, maxLength: number): ValidationResult | true =>
-    obj[key] === undefined || obj[key].length <= maxLength || 
-    error("textIsTruncated", key, { maxLength: maxLength.toString() })
-
-export const requireValue = (obj: any, key: string): ValidationResult | true =>
-    obj[key] !== undefined ||
-    error("requiredValueMissing", key)
-
-export const arrayConstraints = <T>(
-    obj: any,
-    key: string,
-    perItemConstraints: (item: T) => Array<ValidationResult | true>
-): Array<ValidationResult | true> => 
-    obj[key] === undefined ? [] : (obj[key] as Array<T>).flatMap((item, i) => {
-        const results = perItemConstraints(item)
-        results.forEach(result => {
-            if (result !== true) {
-                result.path?.unshift(key)
-                result.path?.unshift(i)
-            }
-        })
-        return results
-    })
-
-export const valueConstraints = <T>(
-    obj: any,
-    key: string,
-    itemConstraints: (item: T) => Array<ValidationResult | true>
-): Array<ValidationResult | true>  => {
-    if (obj[key] === undefined) {
-        return []
-    }
-    const results = itemConstraints(obj[key] as T)
-    results.forEach(result => {
-        if (result !== true) {
-            result.path?.unshift(key)
-        }
-    })
-    return results
-}
-
-
-
-
-
-const onlyFailedConstraints = <T>(arr: Array<T | true>) => arr.filter(it => it !== true ) as Array<T>
-
-const messages = {
-    "requiredValueMissing": "The property \"{key}\" is required.",
-    "arrayIsEmpty": "The property \"{key}\" must have at least one element.",
-
-    "textIsTruncated": "The property \"{key}\" will be truncated, because it is longer than {maxLength} characters.",
-    "textIsTooLong": "The property \"{key}\" may not be longer than {maxLength} characters.",
-    "textHasIncorrectLength": "The property \"{key}\" must have exactly {length} characters.",
-    
-    "lessThanMinimumValue": "The property \"{key}\" may not be less than {minValue}.",
-    "institutionskennzeichenIncorrect": "The property \"{key}\" must consist of 9 digits.",
-    "invoiceNumberIncorrect": "The property \"{key}\" may only contain the characters a-z, A-Z, 0-9 and the separators '-' and '/', though it may not begin or end with a separator.",
-}
-
-export type ValidationPath = Array<string | number>
-
-export type ValidationCode = keyof typeof messages
-
-export enum ValidationResultType { Warning, Error }
-
-export type ValidationResult = {
-  code: ValidationCode
-  type: ValidationResultType
-  path: ValidationPath
-  params?: Record<string, string>
-  message?: string
-}
+export const constraintsSkonto = (s: Skonto) => [
+    isNumber(s, "skontoPercent", 0, 100),
+    isInt(s, "skontoPercent", 0, 1000)
+]
---
+++ tests/validation/utils.spec.ts
@@ -0,0 +1,98 @@
+import { ValidationResultType } from "../../src/validation/index"
+import { 
+    error,
+    isDate, isNumber, isVarchar, isInt, isArray, isRequired, isChar, 
+    isIK, isRechnungsnummer, isTruncatedIfTooLong
+} from "../../src/validation/utils"
+
+describe("validation utils", () => {
+
+    it("isRequired", () => {
+        expect(isRequired({ a: undefined }, "a")).toEqual(error("requiredValueMissing", "a"))
+        expect(isRequired({ a: "I am here" }, "a")).toEqual(undefined)
+    })
+
+    it("isDate", () => {
+        expect(isDate({ a: "not a date" }, "a")).toEqual(error("noDate", "a"))
+        expect(isDate({ a: new Date() }, "a")).toEqual(undefined)
+    })
+
+    it("isNumber", () => {
+        expect(isNumber({ a: "str" }, "a", 0, 1)).toEqual(error("noNumber", "a"))
+        expect(isNumber({ a: -0.01 }, "a", 0, 1)).toEqual(error("numberTooSmall", "a", { min: "0" }))
+        expect(isNumber({ a: 1.000 }, "a", 0, 1)).toEqual(error("numberTooBig", "a", { max: "1" }))
+        expect(isNumber({ a: 0.999 }, "a", 0, 1)).toEqual(undefined)
+    })
+
+    it("isInt", () => {
+        expect(isInt({ a: "str" }, "a", 0, 10)).toEqual(error("noInt", "a"))
+        expect(isInt({ a: 4.12 }, "a", 0, 10)).toEqual(error("noInt", "a"))
+        expect(isInt({ a: -1 }, "a", 0, 10)).toEqual(error("numberTooSmall", "a", { min: "0" }))
+        expect(isInt({ a: 10 }, "a", 0, 10)).toEqual(error("numberTooBig", "a", { max: "10" }))
+        expect(isInt({ a: 9 }, "a", 0, 10)).toEqual(undefined)
+    })
+
+    it("isArray", () => {
+        expect(isArray({ a: "str" }, "a", 0, 10)).toEqual(error("noArray", "a"))
+        expect(isArray({ a: [1] }, "a", 2, 10)).toEqual(error("arrayTooSmall", "a", { minLength: "2" }))
+        expect(isArray({ a: [1,2,3,4,5] }, "a", 2, 4)).toEqual(error("arrayTooLong", "a", { maxLength: "4" }))
+        expect(isArray({ a: [1,2] }, "a", 2, 2)).toEqual(undefined)
+    })
+
+    it("isVarchar", () => {
+        expect(isVarchar({ a: 9 }, "a", 10)).toEqual(error("noString", "a"))
+        expect(isVarchar({ a: "" }, "a", 10)).toEqual(error("textEmpty", "a"))
+        expect(isVarchar({ a: "abc" }, "a", 2)).toEqual(error("textTooLong", "a", { maxLength: "2" }))
+        expect(isVarchar({ a: "abc" }, "a", 3)).toEqual(undefined)
+    })
+
+    it("isChar", () => {
+        expect(isChar({ a: 9 }, "a", 10)).toEqual(error("noString", "a"))
+        expect(isChar({ a: "abc" }, "a", 2)).toEqual(error("textHasIncorrectLength", "a", { length: "2" }))
+        expect(isChar({ a: "a" }, "a", 2)).toEqual(error("textHasIncorrectLength", "a", { length: "2" }))
+        expect(isChar({ a: "ab" }, "a", 2)).toEqual(undefined)
+    })
+
+    it("isIK", () => {
+        expect(isIK({ a: 123456789 }, "a")).toEqual(error("noString", "a"))
+        expect(isIK({ a: "1234567890" }, "a")).toEqual(error("institutionskennzeichenIncorrect", "a"))
+        expect(isIK({ a: "12345678" }, "a")).toEqual(error("institutionskennzeichenIncorrect", "a"))
+        expect(isIK({ a: "123456789" }, "a")).toEqual(undefined)
+    })
+
+    it("isRechnungsnummer", () => {
+        expect(isRechnungsnummer({ a: 123 }, "a")).toEqual(error("noString", "a"))
+        expect(isRechnungsnummer({ a: "-a" }, "a")).toEqual(error("rechnungsnummerIncorrect", "a"))
+        expect(isRechnungsnummer({ a: "/a" }, "a")).toEqual(error("rechnungsnummerIncorrect", "a"))
+        expect(isRechnungsnummer({ a: "a99-" }, "a")).toEqual(error("rechnungsnummerIncorrect", "a"))
+        expect(isRechnungsnummer({ a: "a99-4" }, "a")).toEqual(undefined)
+    })
+
+    it("isTruncatedIfTooLong", () => {
+        expect(
+            isTruncatedIfTooLong(isArray({ a: [1,2,3] }, "a", 2, 2))
+        ).toEqual({
+            code: "arrayTruncated",
+            type: ValidationResultType.Warning,
+            path: ["a"],
+            params: { maxLength: "2" }
+        })
+
+        expect(
+            isTruncatedIfTooLong(isArray({ a: [1,2,3] }, "a", 2, 3))
+        ).toEqual(undefined)
+
+        expect(
+            isTruncatedIfTooLong(isVarchar({ a: "abc" }, "a", 2))
+        ).toEqual({
+            code: "textTruncated",
+            type: ValidationResultType.Warning,
+            path: ["a"],
+            params: { maxLength: "2" }
+        })
+
+        expect(
+            isTruncatedIfTooLong(isVarchar({ a: "ab" }, "a", 2))
+        ).toEqual(undefined)
+    })
+})
