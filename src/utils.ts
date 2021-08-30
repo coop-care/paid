252e96f951826d33456b9c04cfe1c9d9c61b00e5
Tobias Zwick <newton@westnordost.de>
2021-08-30 21:47:04 +0200

    add two utility functions

--- src/utils.ts
+++ src/utils.ts
@@ -15,3 +15,21 @@ export const valuesGroupedBy = <T, K extends string>(items: T[], getKey: (item: 
 
 export const entriesGroupedBy = <T, K extends string>(items: T[], getKey: (item: T) => K) =>
     Object.entries(groupBy(items, getKey)) as [K, T[]][]
+
+/** Same as entriesGroupedBy but allows non-strings as first element (key) in the tuples */
+export const entriesGroupedBy2 = <T, K>(
+    items: T[],
+    getKey: (item: T) => K,
+    getKeyToString: (key: K) => string
+): [K, T[]][] => {
+    const map = new Map<string, K>()
+    return entriesGroupedBy(items, item => {
+        const k = getKey(item)
+        const id = getKeyToString(k)
+        map.set(id, k)
+        return id
+    }).map(([id, items]) => [map.get(id)!, items])
+}
+
+/** Sum the numbers in the given array. Just a shorthand for that awkward reduce function. */
+export const sum = (items: number[]) => items.reduce((a, b) => a + b, 0)
