eb3db60bed6359b0f8badd1f5d4a75214018b633
Michael Kamphausen <mail@michael-kamphausen.de>
2021-09-06 20:22:07 +0200

    encode to and decode from I8 as Uint8Array

---
+++ tests/transcoding/index.spec.ts
@@ -0,0 +1,30 @@
+import { isEncodableI8, encodeI8, decodeI8 } from "../../src/transcoding/din66003drv"
+
+
+describe("transcoding to I8", () => {
+
+    it("string is encodable to I8", () => {
+        expect(isEncodableI8("außergewöhnlich, spécial, særlig")).toEqual(true);
+    });
+
+    it("string is not encodable to I8", () => {
+        expect(isEncodableI8("außergewöhnlich, spécial, zwłaszcza")).toEqual(false);
+    });
+
+    it("encode string to I8", () => {
+        expect(encodeI8("außergewöhnlich, spécial, særlig"))
+        .toEqual(new Uint8Array([
+            97, 117, 126, 101, 114, 103, 101,
+            119, 124, 104, 110, 108, 105, 99,
+            104, 44, 32, 115, 112, 233, 99,
+            105, 97, 108, 44, 32, 115, 230,
+            114, 108, 105, 103
+        ]));
+    });
+
+    it("encode string to I8 and decode back to UTF-8", () => {
+        expect(decodeI8(encodeI8("außergewöhnlich, spécial, særlig")))
+            .toEqual("außergewöhnlich, spécial, særlig");
+    });
+
+});
