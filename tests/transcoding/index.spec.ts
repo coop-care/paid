c92e81cf7f804afe4ce24e4f460704f1e15002c9
Michael Kamphausen <mail@michael-kamphausen.de>
2021-09-21 16:54:52 +0200

    added control characters to I7 (and I8) code tables to be able to encode new lines and tabs

--- tests/transcoding/index.spec.ts
+++ tests/transcoding/index.spec.ts
@@ -4,7 +4,7 @@ import { isEncodableI8, encodeI8, decodeI8 } from "../../src/transcoding/din6600
 describe("transcoding to I8", () => {
 
     it("string is encodable to I8", () => {
-        expect(isEncodableI8("außergewöhnlich, spécial, særlig")).toEqual(true);
+        expect(isEncodableI8("außergewöhnlich, spécial, særlig \n")).toEqual(true);
     });
 
     it("string is not encodable to I8", () => {
@@ -12,19 +12,19 @@ describe("transcoding to I8", () => {
     });
 
     it("encode string to I8", () => {
-        expect(encodeI8("außergewöhnlich, spécial, særlig"))
+        expect(encodeI8("außergewöhnlich, spécial, særlig \n"))
         .toEqual(new Uint8Array([
             97, 117, 126, 101, 114, 103, 101,
             119, 124, 104, 110, 108, 105, 99,
             104, 44, 32, 115, 112, 233, 99,
             105, 97, 108, 44, 32, 115, 230,
-            114, 108, 105, 103
+            114, 108, 105, 103, 32, 10
         ]));
     });
 
     it("encode string to I8 and decode back to UTF-8", () => {
-        expect(decodeI8(encodeI8("außergewöhnlich, spécial, særlig")))
-            .toEqual("außergewöhnlich, spécial, særlig");
+        expect(decodeI8(encodeI8("außergewöhnlich, spécial, særlig \r\n\t")))
+            .toEqual("außergewöhnlich, spécial, særlig \r\n\t");
     });
 
 });
